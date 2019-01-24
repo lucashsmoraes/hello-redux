9fc613a346d8a92d8406ab2fc1700f048d06723d
Lucas <lucashsmoraes@gmail.com>
2019-01-24 14:58:57 -0200

    criado componente counter stateful

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Counter from './counter/counter'
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Counter />
+)
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { render } from 'react-dom'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const renderApp = (NextApp) => {
+    render(
+        <NextApp />,
+      document.querySelector('[data-js="App"]')
+    )
+  }
+  
+  renderApp(App)
+  if (module.hot) {
+    module.hot.accept('./App', () => {
+      const NextApp = require('./App').default
+      renderApp(NextApp)
+    })
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
