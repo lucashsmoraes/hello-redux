25dbf93be1cf620a63dad920f6573c72d8e90eb0
Lucas <lucashsmoraes@gmail.com>
2019-02-02 18:22:52 -0200

    testando Remove counter

--- src/reducers/counters/counters.test.js
+++ src/reducers/counters/counters.test.js
@@ -19,3 +19,17 @@ it('should add new a counter again', () => {
     const after = [0, 1, 0]
     expect(counters(before, action)).to.be.deep.equal(after)
 })
+
+it('should remove a counter', () => {
+    const before = deepFreeze([0, 1, 2])
+    const action = deepFreeze({type: 'REMOVE_COUNTER', index: 1})
+    const after = [0, 2]
+    expect(counters(before, action)).to.be.deep.equal(after)
+})
+
+it('should remove a counter again', () => {
+    const before = deepFreeze([1, 1])
+    const action = deepFreeze({type: 'REMOVE_COUNTER', index: 0})
+    const after = [1]
+    expect(counters(before, action)).to.be.deep.equal(after)
+})
