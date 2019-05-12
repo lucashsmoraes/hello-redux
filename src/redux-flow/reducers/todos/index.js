d775ea8a71b3ff30e2733ae41a80d7c861df3f4c
Lucas <lucashsmoraes@gmail.com>
2019-05-11 23:24:44 -0300

    teste reducer toggle todo

---
+++ src/redux-flow/reducers/todos/index.js
@@ -0,0 +1,31 @@
+import { ADD_TODO } from "./actions";
+import { TOGGLE_TODO } from './actions'
+
+const initialState = []
+
+const todos = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return state.concat({
+        id: action.payload.id,
+        text: action.payload.text,
+        completed: false
+      })
+    case TOGGLE_TODO:
+      return state.map((todo) => {
+        if (todo.id !== action.payload.id) {
+          return todo
+        }
+        return {
+          ...todo,
+          completed: !todo.completed
+        }
+        
+      })
+      default:
+        return 
+  }
+
+}
+
+export default todos;
